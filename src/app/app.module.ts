ab6da9cf1384f0fd533611562d514b3a569f7e01
David Rodriguez <dissonant.tech@gmail.com>
2017-11-15 15:08:56 -0500

    Add Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -10,7 +11,18 @@ import { TerminalComponent } from './terminal/terminal.component';
 import { OsxtitlebarComponent } from './osxtitlebar/osxtitlebar.component';
 import { CardComponent } from './card/card.component';
 import { CardService } from './card/card.service';
+import { ArticleComponent } from './pages/article/article.component';
+import { CardpageComponent } from './pages/cardpage/cardpage.component';
 
+
+const appRoutes: Routes = [
+  { path: 'article/:id',      component: ArticleComponent },
+  { path: '',
+    component: CardpageComponent,
+    pathMatch: 'full'
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
@@ -19,8 +31,14 @@ import { CardService } from './card/card.service';
     TerminalComponent,
     OsxtitlebarComponent,
     CardComponent,
+    ArticleComponent,
+    CardpageComponent,
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule
